1355e4febec2e70cbf6c00909aff82d37b41824f
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-11-28 09:56:08 +0300

    подготовка профиля

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,13 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
 
 function App() {
     const { theme } = useTheme();
     const dispatch = useDispatch();
+    const _inited = useSelector(getUserInited);
 
     useEffect(() => {
         dispatch(userActions.initAuthData());
@@ -21,7 +22,7 @@ function App() {
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
-                    <AppRouter />
+                    {_inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
