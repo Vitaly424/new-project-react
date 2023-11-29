9c214e8a41a6bbbf0ae08a747d7028df115da980
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-11-29 18:44:40 +0300

    добавил комментарии

---
+++ src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
@@ -0,0 +1,54 @@
+import {
+    createEntityAdapter,
+    createSlice, EntityState, PayloadAction,
+} from '@reduxjs/toolkit';
+import { Comment } from 'entities/Comment';
+import { StateSchema } from 'app/providers/StoreProvider';
+import {
+    ArticleDetailsCommentsSchema,
+} from '../types/ArticleDetailsCommentsSchema/ArticleDetailsCommentsSchema';
+import {
+    fetchCommentsByArticleId,
+} from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+
+const commentsAdapter = createEntityAdapter<Comment>({
+    selectId: (comment) => comment.id,
+});
+
+export const getArticleComments = commentsAdapter.getSelectors<StateSchema>(
+    (state) => state.articleDetailsComments || commentsAdapter.getInitialState(),
+);
+
+const articleDetailsCommentsSlice = createSlice({
+    name: 'articleDetailsComments',
+    initialState: commentsAdapter.getInitialState<ArticleDetailsCommentsSchema>({
+        isLoading: false,
+        error: undefined,
+        ids: ['1', '2'],
+        entities: {},
+    }),
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCommentsByArticleId.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(fetchCommentsByArticleId.fulfilled, (
+                state,
+                action: PayloadAction<Comment[]>,
+            ) => {
+                state.isLoading = false;
+                commentsAdapter.setAll(state, action.payload);
+            })
+            .addCase(fetchCommentsByArticleId.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
+});
+
+export const { reducer: articleDetailsCommentsReducer } = articleDetailsCommentsSlice;
+export const { actions: articleDetailsCommentsActions } = articleDetailsCommentsSlice;
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -3,15 +3,39 @@ import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { ArticleDetails } from 'entities/Article';
 import { useParams } from 'react-router-dom';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentList } from 'entities/Comment';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { articleDetailsReducer } from 'entities/Article/model/slice/articleDetailsSlice';
+import { useSelector } from 'react-redux';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import {
+    fetchCommentsByArticleId,
+} from 'pages/ArticleDetailsPage/model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+import { getArticleCommentsIsLoading } from '../../model/selectors/comments';
+import { articleDetailsCommentsReducer, getArticleComments } from '../../model/slices/articleDetailsCommentsSlice';
+import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
     className?: string;
 }
 
+const reducers: ReducersList = {
+    articleDetailsComments: articleDetailsCommentsReducer,
+};
+
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
+    const dispatch = useAppDispatch();
     const { id } = useParams<{id: string}>();
     const { t } = useTranslation('article-details');
+    const comments = useSelector(getArticleComments.selectAll);
+    const isLoading = useSelector(getArticleCommentsIsLoading);
+
+    useInitialEffect(() => {
+        dispatch(fetchCommentsByArticleId(id));
+    });
 
     if (!id) {
         return (
@@ -22,9 +46,19 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     }
 
     return (
-        <div className={classNames('', {}, [className])}>
-            <ArticleDetails id={id} />
-        </div>
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <div className={classNames('', {}, [className])}>
+                <ArticleDetails id={id} />
+                <Text
+                    className={cls.commentTitle}
+                    title={t('Комментарии')}
+                />
+                <CommentList
+                    isLoading={isLoading}
+                    comments={comments}
+                />
+            </div>
+        </DynamicModuleLoader>
     );
 };
 
