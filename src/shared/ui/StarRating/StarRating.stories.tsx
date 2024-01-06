4c5fb54da67060bff7392540dc751b37d2b8d5d3
Vitaly <v1t19982@yandex.ru>
2024-01-06 16:26:34 +0300

    доработал eslint плагин

--- src/shared/ui/StarRating/StarRating.stories.tsx
+++ src/shared/ui/StarRating/StarRating.stories.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Page } from './Page';
+import { StarRating } from './StarRating';
 
 export default {
-    title: 'widgets/Page',
-    component: Page,
+    title: 'shared/StarRating',
+    component: StarRating,
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-} as ComponentMeta<typeof Page>;
+} as ComponentMeta<typeof StarRating>;
 
-const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
+const Template: ComponentStory<typeof StarRating> = (args) => <StarRating {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {};
